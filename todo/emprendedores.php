986a231088afa5e82d3bc08b7f02e6dd4ca6ae82
Oscar255l <patinnoscar@gmail.com>
2025-03-24 11:30:55 -0500

    lo logre

--- todo/emprendedores.php
+++ todo/emprendedores.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.1/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style_productos.css">
+    <link rel="stylesheet" href="css/style_emprendedores.css">
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
     <script defer src="js/menu.js"></script>
